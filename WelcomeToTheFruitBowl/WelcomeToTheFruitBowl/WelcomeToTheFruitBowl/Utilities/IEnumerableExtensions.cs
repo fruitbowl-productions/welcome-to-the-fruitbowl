f4a91446fc29c5d0db8f5276b1f9d30b6feb100d
Joshua Grosso <jgrosso1@asu.edu>
2016-04-30 09:20:24 -0700

    Add ReverseInPlace() method for IEnumerable.

---
+++ WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Utilities/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace WelcomeToTheFruitBowl.Utilities
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> ReverseInPlace<T>(this IEnumerable<T> list)
+        {
+            var newList = new List<T>(list);
+            newList.Reverse();
+            return newList;
+        }
+    }
+}
