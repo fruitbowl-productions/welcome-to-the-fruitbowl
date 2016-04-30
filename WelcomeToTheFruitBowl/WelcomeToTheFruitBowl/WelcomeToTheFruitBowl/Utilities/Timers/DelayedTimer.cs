81fcd82a05477c54573ffdc8342b43db6df51e5a
Joshua Grosso <jgrosso1@asu.edu>
2016-04-30 10:35:26 -0700

    Add Timers

---
+++ WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Utilities/Timers/DelayedTimer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace WelcomeToTheFruitBowl.Utilities.Timers
+{
+    public class DelayedTimer : Timer
+    {
+        private readonly Action offAction;
+        private TimeSpan actionTime;
+
+        public DelayedTimer(Action action, TimeSpan delay, Action offAction) : base(action, delay)
+        {
+            this.offAction = offAction;
+            actionTime = TimeSpan.Zero;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Current += gameTime.ElapsedGameTime;
+
+            if (Current >= Delay)
+            {
+                actionTime += gameTime.ElapsedGameTime;
+
+                if (Current < Delay + actionTime) return;
+                Action.Invoke();
+                Current = TimeSpan.Zero;
+                actionTime = TimeSpan.Zero;
+            }
+            else
+            {
+                offAction.Invoke();
+            }
+        }
+    }
+}
---
+++ WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Utilities/Timers/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace WelcomeToTheFruitBowl.Utilities.Timers
+{
+    public class Timer
+    {
+        protected readonly Action Action;
+
+        protected TimeSpan Delay;
+        protected TimeSpan Current;
+
+        public Timer(Action action, TimeSpan delay)
+        {
+            this.Action = action;
+            this.Delay = delay;
+            Current = TimeSpan.Zero;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            Current += gameTime.ElapsedGameTime;
+            if (Current < Delay) return;
+            Action.Invoke();
+            Current = TimeSpan.Zero;
+        }
+    }
+}
