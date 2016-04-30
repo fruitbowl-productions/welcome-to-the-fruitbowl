02a5c43cd7c35c96f21fd482e019575ff3149535
Keaten Wood <klwood2@asu.edu>
2016-04-30 09:14:05 -0700

    Add Console Class

---
+++ WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Engine/Console.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace WelcomeToTheFruitBowl.Engine
+{
+    public class Console
+    {
+        private SpriteFont font;
+        private int height;
+        private int width;
+        private Vector2 position;
+        private List<string> linesList;
+        private string usertype;
+
+        public Console(SpriteFont spritefont)
+        {
+            font = spritefont;
+            height = Screen.Height;
+            width = Screen.Width;
+            position = new Vector2(0, 0);
+            linesList = new List<string>();
+        }
+
+        public void Update()
+        {
+            foreach (var item in linesList.Reverse())
+            {
+                
+            }
+            // Update User Type
+            // Update Orientation
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw Previous Type 
+            // Draw Console Type 
+            // Draw User Type
+        }
+
+
+    }
+}
