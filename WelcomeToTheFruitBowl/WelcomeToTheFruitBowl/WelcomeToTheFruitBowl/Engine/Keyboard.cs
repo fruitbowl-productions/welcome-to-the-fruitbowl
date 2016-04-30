f740fb87002777f2b15c8b72cdafe53ed59e8302
Joshua Grosso <jgrosso1@asu.edu>
2016-04-30 09:38:55 -0700

    Add IsAnyKeydown() method.

--- WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Engine/Keyboard.cs
+++ WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Engine/Keyboard.cs
@@ -16,6 +16,13 @@ namespace WelcomeToTheFruitBowl.Engine
         public static bool IsKeyDown(Keys key) => currentState.IsKeyDown(key);
         public static bool IsKeyUp(Keys key) => currentState.IsKeyUp(key);
 
+        public static bool IsAnyKeyDown()
+        {
+            // http://xboxforums.create.msdn.com/forums/p/49933/667071.aspx
+            return currentState.GetPressedKeys().Length == 0 ||
+                   (currentState.GetPressedKeys().Length == 1 && currentState.GetPressedKeys()[0] == Keys.None);
+        }
+
         public static void Update()
         {
             previousState = currentState;
