4f1e9ecdca7241d47dbfa2a73801fd441e76c5f7
Keaten Wood <klwood2@asu.edu>
2016-04-30 20:27:09 -0700

    Blueprinted Player and Stats Class

---
+++ WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WelcomeToTheFruitBowl
+{
+    public class Player
+    {
+        private string name;
+
+        private enum race
+        {
+            Human,
+            Elf  
+        }
+
+        private Stats stats;
+
+        public Player()
+        {
+            
+        }
+    }
+}
---
+++ WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Stats.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WelcomeToTheFruitBowl
+{
+    class Stats
+    {
+        public int Health;
+        public int CurrentHealth;
+        public int Mana;
+        public int CurrentMana;
+        private int strength;
+        private int currentStrengthBonus;
+
+        public int Strength
+        {
+            get { return strength + currentStrengthBonus; }
+            set { strength = value; }
+        }
+
+        private int speed;
+        private int currentSpeedBonus;
+
+        public int Speed
+        {
+            get { return speed + currentSpeedBonus; }
+            set { speed = value; }
+        }
+
+        public Stats()
+        {
+            
+        }
+    }
+}
