45dc7bed5e86e51df0f93fa524e7fdf61a16fae5
Joshua Grosso <jgrosso1@asu.edu>
2016-05-04 14:43:48 -0700

    Remove unnecessary extension methods.

--- WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Utilities/Extensions.cs
+++ WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/WelcomeToTheFruitBowl/Utilities/Extensions.cs
@@ -11,39 +11,5 @@ namespace WelcomeToTheFruitBowl.Utilities
             newList.Reverse();
             return newList;
         }
-
-        public static string Combine(this IEnumerable<string> list)
-        {
-            var result = new StringBuilder();
-
-            foreach (var item in list)
-            {
-                result.Append(item);
-            }
-
-            return result.ToString();
-        }
-
-        public static IEnumerable<string> SplitEvery(this string thisString, int n)
-        {
-            var tempString = string.Copy(thisString);
-            var result = new List<string>();
-
-            while (tempString != "")
-            {
-                if (tempString.Length > n)
-                {
-                    result.Add(tempString.Substring(0, n));
-                    tempString = tempString.Substring(n);
-                }
-                else
-                {
-                    result.Add(tempString);
-                    tempString = "";
-                }
-            }
-
-            return result;
-        }
     }
 }
